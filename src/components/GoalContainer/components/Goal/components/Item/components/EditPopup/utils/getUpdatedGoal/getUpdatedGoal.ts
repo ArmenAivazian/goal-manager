3f7d684cbc614c143a5422c4e209f2e84d15fefc
Armen <armen.aivazian@amo.tech>
2023-08-17 10:20:41 +0300

    feature/change-importance

--- src/components/GoalContainer/components/Goal/components/Item/components/EditPopup/utils/getUpdatedGoal/getUpdatedGoal.ts
+++ src/components/GoalContainer/components/Goal/components/Item/components/EditPopup/utils/getUpdatedGoal/getUpdatedGoal.ts
@@ -14,54 +14,54 @@ export function getUpdatedGoal(
   prevGoal: GoalType,
   id: string,
   value: GoalNewValue,
-  newImportance?: number
+  newImportanceForNeighbor?: number
 ) {
+  if (newImportanceForNeighbor) {
+    const importance = Number(prevGoal.importance) || 0;
+    const newValue = importance + newImportanceForNeighbor;
+
+    return editGoal(prevGoal, "importance", newValue.toString());
+  }
+
   if (prevGoal.id === id) {
     if (action === "add") return addNewSubGoal(prevGoal, value);
 
-    if (action?.type === "edit") {
-      return editGoal(prevGoal, action.field, value);
-    }
+    return editGoal(prevGoal, action.field, value);
   }
 
-  if (newImportance) return { ...prevGoal, importance: newImportance };
-
   if (prevGoal.children) {
-    const isParent = prevGoal.children.some((goal) => goal.id === id);
-    const isImportanceEdit =
-      typeof action !== "string" && action?.field === "importance";
+    const isActionString = typeof action === "string";
+    const isImportanceEdit = !isActionString && action.field === "importance";
+    let children: GoalType[] = [];
 
-    if (isImportanceEdit && isParent) {
-      const totalImportance = prevGoal.children.reduce((acc, goal) => {
-        if (goal.id === id) return acc + Number(value);
+    if (isImportanceEdit) {
+      const isParent = prevGoal.children.some((child) => child.id === id);
+      if (isParent) {
+        const totalImportance = prevGoal.children.reduce(
+          (acc, { id: childId, importance }) => {
+            if (childId === id) return acc + Number(value);
+            return acc + (Number(importance) || 0);
+          },
+          0
+        );
+        const different = 100 - totalImportance;
+        const forChild = different / (prevGoal.children.length - 1);
 
-        return acc + (goal.importance || 0);
-      }, 0);
+        children = prevGoal.children.map((goal) => {
+          const isTargetGoal = goal.id === id;
 
-      const balance =
-        (100 - totalImportance) / (prevGoal.children.length - 100);
+          if (isTargetGoal) return getUpdatedGoal(action, goal, id, value);
 
-      return {
-        ...prevGoal,
-        children: prevGoal.children.map(
-          (goal): GoalType =>
-            getUpdatedGoal(
-              action,
-              goal,
-              id,
-              value,
-              Number(goal.importance || 0) + balance
-            )
-        ),
-      };
+          return getUpdatedGoal(action, goal, id, value, forChild);
+        });
+      }
+    } else {
+      children = prevGoal.children.map((goal) =>
+        getUpdatedGoal(action, goal, id, value)
+      );
     }
 
-    return {
-      ...prevGoal,
-      children: prevGoal.children.map(
-        (goal): GoalType => getUpdatedGoal(action, goal, id, value)
-      ),
-    };
+    return { ...prevGoal, children };
   }
 
   return prevGoal;
