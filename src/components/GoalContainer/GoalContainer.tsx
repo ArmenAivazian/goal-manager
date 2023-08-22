0509fa3076488dc6ce415fddaaed074b96f8b3f7
Armen <armen.aivazian@amo.tech>
2023-08-22 11:24:17 +0300

    fix focus mode | change fileds

--- src/components/GoalContainer/GoalContainer.tsx
+++ src/components/GoalContainer/GoalContainer.tsx
@@ -5,12 +5,21 @@ import { Goal } from "./components/Goal";
 import { GoalContext } from "../../contexts/Goal";
 import { Item } from "./components/Goal/components/Item";
 import { useGetContext } from "../../hooks/useContext";
+import { useEffect } from "react";
+import { getGoalById } from "../../utils";
 
 export function GoalContainer({
   selectedGoal,
   setSelectedGoal,
 }: GoalContainerProps) {
   const initGoal = useGetContext(GoalContext);
+
+  useEffect(() => {
+    if (selectedGoal && initGoal) {
+      setSelectedGoal(getGoalById(selectedGoal.id, initGoal));
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [initGoal]);
 
   const goal = selectedGoal || initGoal;
 
