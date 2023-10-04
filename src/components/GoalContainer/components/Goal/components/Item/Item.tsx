a2d3436deff17e7b8f6534bd3ef7ca6ca0cf765a
Armen <armen.aivazian@amo.tech>
2023-10-04 23:16:16 +0300

    fix bug with overwidth goal 3.0

--- src/components/GoalContainer/components/Goal/components/Item/Item.tsx
+++ src/components/GoalContainer/components/Goal/components/Item/Item.tsx
@@ -13,7 +13,7 @@ export function Item({ id, name, progress, onDbClick, ...params }: ItemProps) {
   const ref = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    if (Number(ref?.current?.offsetWidth) < 35) setIsTextVisible(false);
+    if (Number(ref?.current?.offsetWidth) < 40) setIsTextVisible(false);
   }, [params.importance]);
 
   return (
